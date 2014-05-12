0d81ff5917bd2c0d2619917d280cffe80fccdd8a
Kevin Alford <zeroeth@pixelflow.org>
2014-05-12 18:27:15 -0500

    More things

--- my_class.rb
+++ my_class.rb
@@ -1,9 +1,17 @@
 class MyClass
   def a
     puts "untested!"
+    puts "more lines"
+    puts "more lines"
+    puts "more lines"
+    puts "more lines"
   end
 
   def b
     puts "TESTED"
+  end
+
+  def c
+    puts "untested 2!"
   end
 end
